9d0c198dbd46361eb783b505d76c017ba9affcf6
Shimojima <sigkalra8888@gmail.com>
2019-07-16 16:25:52 +0900

    [New] 水中操作用のスクリプト作成

---
+++ Team3Project/Assets/Shimojima/WaterIN.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterIN : MonoBehaviour
+{
+    public static float oxygen;
+    [SerializeField]
+    private float maxOx;
+
+    public void WaterInPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+    }
+
+    public void OxygenUse()
+    {
+        if(oxygen <= 0) { return; }
+        oxygen--;
+    }
+
+    public void OxygenRevive()
+    {
+        oxygen = maxOx;
+    }
+}
