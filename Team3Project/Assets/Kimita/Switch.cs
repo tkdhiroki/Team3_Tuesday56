66ff24c31f1839a51fbb4cfc8a1472ef121cf9b4
KimitaSora <kamaaya1118@gmail.com>
2019-08-27 19:18:04 +0900

    移動床とスイッチのスクリプトを作成

---
+++ Team3Project/Assets/Kimita/Switch.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public enum OwnSwitch
+    {
+        Floor1 = 0,
+        Floor2  ,
+        Floor3 ,
+        Floor4 ,
+        Floor5 ,
+        Door2, 
+    }
+    public OwnSwitch own;
+    [SerializeField]
+    private GameObject Floor;
+    bool enable;
+    GameObject Camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&collision.gameObject.tag == "Player"&&!enable)
+        {
+            switch (own)
+            {
+                case OwnSwitch.Floor1:
+                    StartCoroutine(Floor1Up());
+                    enable = true;
+                    break;
+                case OwnSwitch.Floor2:
+                    StartCoroutine(FloorUp());
+                    enable = true;
+                    break;
+                case OwnSwitch.Floor3:
+                    StartCoroutine(FloorUp());
+                    enable = true;
+                    break;
+                case OwnSwitch.Floor4:
+                    StartCoroutine(FloorUp());
+                    enable = true;
+                    break;
+                case OwnSwitch.Floor5:
+                    StartCoroutine(FloorUp());
+                    enable = true;
+                    break;
+                case OwnSwitch.Door2:
+                    StartCoroutine(DoorDown());
+                    enable = true;
+                    break;
+            }
+        }
+    }
+    
+    IEnumerator Floor1Up()
+    {
+        float up = 0;
+        float now = Floor.transform.position.y;
+
+        while (up < 7)
+        {
+            yield return new WaitForSeconds(Time.deltaTime); 
+            Floor.transform.position = new Vector3(Floor.transform.position.x, now + up, Floor.transform.position.z);
+            up += 0.1f;
+        }
+        up = 7;
+        Floor.transform.position = new Vector3(Floor.transform.position.x, now + up, Floor.transform.position.z);
+    }
+    IEnumerator FloorUp()
+    {
+        float up = 0;
+        float now = Floor.transform.position.y;
+
+        while (up < 9)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            Floor.transform.position = new Vector3(Floor.transform.position.x, now + up, Floor.transform.position.z);
+            up += 0.1f;
+        }
+        up = 9;
+        Floor.transform.position = new Vector3(Floor.transform.position.x, now + up, Floor.transform.position.z);
+
+        
+    }
+    IEnumerator DoorDown()
+    {
+        float up = 0;
+        float now = Floor.transform.position.y;
+
+        while (up < 3.5f)
+        {
+            yield return new WaitForSeconds(0.05f);
+            Floor.transform.position = new Vector3(Floor.transform.position.x, now - up, Floor.transform.position.z);
+            up += 0.1f;
+        }
+        up = 3.5f;
+        Floor.transform.position = new Vector3(Floor.transform.position.x, now - up, Floor.transform.position.z);
+
+    }
+}
---
+++ Team3Project/Assets/TestPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    Rigidbody2D rgbd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - 0.05f, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 0.05f, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rgbd.AddForce(transform.up*100);
+        }
+    }
+}
