d1881cba5ea1cae3bbbe0aef91602636ace8d24e
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-08-27 17:20:53 +0900

    TIMEOUT

---
+++ Team3Project/Assets/Syuu/Syuu/TIME.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class TIME : MonoBehaviour
+{
+    public Text TIMETEXT; // Textオブジェクト
+    public float TIMEOUT;
+    // 初期化
+    void Start()
+    {
+    }
+
+    // 更新
+    void Update()
+    {
+        TIMEOUT = TIMEOUT - 1 * Time.deltaTime;
+        if (TIMEOUT > 0) {
+            TIMETEXT.text = " " + (int)TIMEOUT;
+        }
+       
+
+    }
+}
