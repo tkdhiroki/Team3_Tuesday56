93cd174337310fc8e5655cb55ff56e4fac4185f1
高田将志 <masapo.0315@icloud.com>
2019-09-03 16:12:30 +0900

    ギミックの上げなおし

---
+++ Team3Project/Assets/Takada/Scripts/Carvament.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carvament : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
